fb8441b037da5d4f7221af48d9f4a5bf2bbbb904
sanjaynakka <sanjaynakka@192.168.0.15>
2019-02-20 23:04:34 -0600

    FirstCommit

---
+++ FirstFromMac/src/com/java/start/Sample.java
@@ -0,0 +1,28 @@
+package com.java.start;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, I am the first program from the Macbook Pro!");
+		
+		//System.getProperty("webdriver.chrome.driver", "Users⁩\\sanjaynakka⁩\\Downloads⁩\\googlechrome.dmg");
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://google.com");
+		
+		Thread.sleep(5000);
+		
+		driver.close();
+		
+
+	}
+
+}
